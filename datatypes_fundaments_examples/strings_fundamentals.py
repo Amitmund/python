0c3e4ff5f7701a282a13f0451e3679ea715a3e18
Amit Mund <Amitmund@users.noreply.github.com>
2025-05-30 08:15:36 +0530

    Update strings_fundamentals.py

--- datatypes_fundaments_examples/strings_fundamentals.py
+++ datatypes_fundaments_examples/strings_fundamentals.py
@@ -1,2 +1,55 @@
+# --------------------------------------------------------------------------------
+
+# Using of a variable in python.
 message = "Welcome to Python!"
 print(message)
+
+# --------------------------------------------------------------------------------
+
+# .title() is a method
+# A method is an action that python can perform on a piece of data
+
+name = "amit mund"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+
+# --------------------------------------------------------------------------------
+
+# f (Formatted) string in python
+first_name = "amit"
+last_name = "mund"
+full_name = f "{first_name} {last_name}"
+print(fullname.title())
+print(f"Hello, {full_name.title()}!")
+
+
+# --------------------------------------------------------------------------------
+
+# IMP
+# t-strings in python 3.14
+name = "amit mund"
+message = t"Hello,{name}!"
+print(message)
+message = t"Hello,{name}.title()!"
+print(message)
+
+
+# --------------------------------------------------------------------------------
+# r-string ("raw string")
+# Tell the python interpreter to treat backslashes \ as literal characters.
+
+print("Hello \t World")
+print(r"Hello \t World")
+
+valid_raw_string = r"C:\path\to\folder" + "\\"
+print(valid_raw_string) # Output: C:\path\to\folder\
+
+valid_raw_string = r"C:\path\to\folder" + "\\"
+print(valid_raw_string) # Output: C:\path\to\folder\
+
+valid_raw_string = r"C:\path\to\folder" + "\\"
+print(valid_raw_string) # Output: C:\path\to\folder\
+
+# --------------------------------------------------------------------------------
