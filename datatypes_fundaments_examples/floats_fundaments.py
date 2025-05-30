f8d274ead067d7ae8d2ae8e552a48e4e56a41f21
Amit Mund <Amitmund@users.noreply.github.com>
2025-05-30 09:31:35 +0530

    Create floats_fundaments.py

---
+++ datatypes_fundaments_examples/floats_fundaments.py
@@ -0,0 +1,12 @@
+0.1+0.2
+
+
+
+# When you divide any two numbers, even if they are integers that result in a whold number, you will always get a `float`.
+2/4 # its a float
+
+
+
+
+# This is also a valid way of writing integer.
+age_of_our_universe = 14_000_000_000
