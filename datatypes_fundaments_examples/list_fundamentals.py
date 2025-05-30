ab1be9da4e255a0c2ae2dfd5f842f33a78a9f071
Amit Mund <Amitmund@users.noreply.github.com>
2025-05-30 09:22:57 +0530

    Create list_fundamentals.py

---
+++ datatypes_fundaments_examples/list_fundamentals.py
@@ -0,0 +1,18 @@
+languages = [ "python","c","javascript","java"]
+print(languages)
+print(languages[0])
+print(languages[2].title())
+print(languages[-1]) #return the last object
+
+
+
+
+# Lists are mutable, which mean you can update the list.
+
+bike = ['honda', 'yamaha', 'suzuki']
+print(bike)
+bike[0] = 'hero'
+print(bike)
+
+
+
